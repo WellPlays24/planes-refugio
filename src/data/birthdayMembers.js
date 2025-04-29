74c4fd4f15b15955d86ab669963e4f775c52f1cd
WellPlays24 <wellington.castillocisf@gmail.com>
2025-04-29 00:15:06 -0500

    fechas corregidas

--- src/data/birthdayMembers.js
+++ src/data/birthdayMembers.js
@@ -36,32 +36,32 @@ import bernal from '../assets/birthdays/bernal.png';
 const birthdayMembers = [
   { id: 0, name: 'Wellington Castillo', birthday: '1999-05-31', place: 'Machala', image: well }, //   8 de mayo
   { id: 1, name: 'Rebecca Moysam', birthday: '2003-05-09', place: 'Guayaquil', image: becca }, //   8 de mayo
-  { id: 2, name: 'Damiana Belén Herdoiza Ramirez', birthday: '2005-11-06', place: 'Guayaquil', image: damiana }, //   7 de noviembre
-  { id: 3, name: 'Ángel PK', birthday: '1998-05-11', place: 'Guayaquil', image: angel }, //   12 de mayo
-  { id: 4, name: 'Líder Andrés Calle Pinzón', birthday: '1998-12-12', place: 'Quito', image: lider }, //   13 de diciembre
-  { id: 5, name: 'Marichuy Robles', birthday: '1998-07-02', place: 'Machala', image: marichuy }, //   3 de julio
-  { id: 6, name: 'Josué Miguel Suescum Segurs', birthday: '2005-07-19', place: 'Santa Elena', image: josue }, //   20 de julio
-  { id: 7, name: 'Roberta Cedeño', birthday: '2000-04-16', place: 'San Vicente', image: roberta }, //   17 de abril
-  { id: 8, name: 'Gloria Grimalda Cabrera Armijos', birthday: '2004-03-02', place: 'El Guabo', image: gloria }, //   3 de marzo
-  { id: 9, name: 'Jordy Cali Marcillo', birthday: '2000-05-19', place: 'Guayaquil', image: jordy }, //   20 de mayo
-  { id: 10, name: 'Danny De La Torre', birthday: '2000-04-11', place: 'Guayaquil', image: danny }, //   12 de abril
-  { id: 11, name: 'Dayanara Alava', birthday: '1999-03-22', place: 'Manta y Quito', image: dayanara }, //   23 de marzo
-  { id: 12, name: 'Gustavo Samuel Sandoya Sánchez', birthday: '2000-08-15', place: 'Guayaquil', image: gustavo }, //   16 de agosto
-  { id: 13, name: 'Elvys Anderson Donoso Cedeño', birthday: '2007-06-27', place: 'Manabí-El Carmen', image: elvys }, //   28 de junio
-  { id: 14, name: 'Lanita Castillo', birthday: '2005-04-02', place: 'Quito', image: lanita }, //  3 de abril
-  { id: 15, name: 'Ana Cristina Villacres López', birthday: '2000-10-11', place: 'Santo Domingo', image: anaCristina }, //   12 de octubre
-  { id: 16, name: 'Brigitte Dayana Reina', birthday: '2006-07-10', place: 'Manta', image: dayana }, //  11 de julio
-  { id: 17, name: 'Kevin Esteven Cevallos Baque', birthday: '1995-09-27', place: 'Guayaquil', image: kevin }, //  28 de septiembre
-  { id: 18, name: 'Liz Madelyne Tumbaco Peñafiel', birthday: '1999-04-19', place: 'Guayaquil', image: lizMadelyne }, //  20 de abril
-  { id: 19, name: 'Allison Nicole Toapanta Arguello', birthday: '2004-09-09', place: 'Guayaquil', image: allison }, //  10 de septiembre
-  { id: 20, name: 'Krizia Saltos', birthday: '2002-12-25', place: 'Quito', image: krizia }, //   26 de diciembre
-  { id: 21, name: 'Lilibeth Maribel Salazar', birthday: '2006-07-30', place: 'Vinces, Los Ríos', image: lilibeth }, //   31 de julio
-  { id: 22, name: 'Noemi Yamilet Mora Villon', birthday: '2005-05-04', place: 'Guayaquil', image: noemi }, //   5 de mayo
-  { id: 23, name: 'Lizbeth Yasmin Gavilanes Quingaguano', birthday: '2000-11-15', place: 'Riobamba', image: lizbeth }, //   16 de noviembre
-  { id: 24, name: 'Axel Esteban Clemente Escandón', birthday: '1998-11-22', place: 'Salinas', image: axel }, //   23 de noviembre
-  { id: 25, name: 'Greece Greece', birthday: '2002-07-04', place: 'Manta', image: greece }, //   5 de julio
-  { id: 26, name: 'Melani Joza', birthday: '2002-01-03', place: 'El Carmen', image: melani }, //   4 de enero
-  { id: 27, name: 'Anthony Bernal', birthday: '2002-04-25', place: 'Manta', image: bernal }, //   26 de abril
+  { id: 2, name: 'Damiana Belén Herdoiza Ramirez', birthday: '2005-11-08', place: 'Guayaquil', image: damiana }, //   7 de noviembre
+  { id: 3, name: 'Ángel PK', birthday: '1998-05-13', place: 'Guayaquil', image: angel }, //   12 de mayo
+  { id: 4, name: 'Líder Andrés Calle Pinzón', birthday: '1998-12-14', place: 'Quito', image: lider }, //   13 de diciembre
+  { id: 5, name: 'Marichuy Robles', birthday: '1998-07-04', place: 'Machala', image: marichuy }, //   3 de julio
+  { id: 6, name: 'Josué Miguel Suescum Segurs', birthday: '2005-07-21', place: 'Santa Elena', image: josue }, //   20 de julio
+  { id: 7, name: 'Roberta Cedeño', birthday: '2000-04-18', place: 'San Vicente', image: roberta }, //   17 de abril
+  { id: 8, name: 'Gloria Grimalda Cabrera Armijos', birthday: '2004-03-04', place: 'El Guabo', image: gloria }, //   3 de marzo
+  { id: 9, name: 'Jordy Cali Marcillo', birthday: '2000-05-21', place: 'Guayaquil', image: jordy }, //   20 de mayo
+  { id: 10, name: 'Danny De La Torre', birthday: '2000-04-13', place: 'Guayaquil', image: danny }, //   12 de abril
+  { id: 11, name: 'Dayanara Alava', birthday: '1999-03-24', place: 'Manta y Quito', image: dayanara }, //   23 de marzo
+  { id: 12, name: 'Gustavo Samuel Sandoya Sánchez', birthday: '2000-08-17', place: 'Guayaquil', image: gustavo }, //   16 de agosto
+  { id: 13, name: 'Elvys Anderson Donoso Cedeño', birthday: '2007-06-29', place: 'Manabí-El Carmen', image: elvys }, //   28 de junio
+  { id: 14, name: 'Lanita Castillo', birthday: '2005-04-04', place: 'Quito', image: lanita }, //  3 de abril
+  { id: 15, name: 'Ana Cristina Villacres López', birthday: '2000-10-13', place: 'Santo Domingo', image: anaCristina }, //   12 de octubr
+  { id: 16, name: 'Brigitte Dayana Reina', birthday: '2006-07-12', place: 'Manta', image: dayana }, //  11 de julio
+  { id: 17, name: 'Kevin Esteven Cevallos Baque', birthday: '1995-09-29', place: 'Guayaquil', image: kevin }, //  28 de septiembre
+  { id: 18, name: 'Liz Madelyne Tumbaco Peñafiel', birthday: '1999-04-21', place: 'Guayaquil', image: lizMadelyne }, //  20 de abril
+  { id: 19, name: 'Allison Nicole Toapanta Arguello', birthday: '2004-09-11', place: 'Guayaquil', image: allison }, //  10 de septiembre
+  { id: 20, name: 'Krizia Saltos', birthday: '2002-12-27', place: 'Quito', image: krizia }, //   26 de diciembre
+  { id: 21, name: 'Lilibeth Maribel Salazar', birthday: '2006-08-01', place: 'Vinces, Los Ríos', image: lilibeth }, //   31 de julio
+  { id: 22, name: 'Noemi Yamilet Mora Villon', birthday: '2005-05-06', place: 'Guayaquil', image: noemi }, //   5 de mayo
+  { id: 23, name: 'Lizbeth Yasmin Gavilanes Quingaguano', birthday: '2000-11-17', place: 'Riobamba', image: lizbeth }, //   16 de noviembre
+  { id: 24, name: 'Axel Esteban Clemente Escandón', birthday: '1998-11-24', place: 'Salinas', image: axel }, //   23 de noviembre
+  { id: 25, name: 'Greece Greece', birthday: '2002-07-06', place: 'Manta', image: greece }, //   5 de julio
+  { id: 26, name: 'Melani Joza', birthday: '2002-01-05', place: 'El Carmen', image: melani }, //   4 de enero
+  { id: 27, name: 'Anthony Bernal', birthday: '2002-04-27', place: 'Manta', image: bernal }, //   26 de abril
   // Aquí puedes añadir más miembros con cumpleaños si es necesario.
 ];
 
